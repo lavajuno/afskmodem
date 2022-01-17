7e15704e8bf79f52c02db092a3d8edfa71ca1dbb
Juno <jvmeifert@gmail.com>
2022-01-17 16:08:41 -0500

    Updated demos

--- rx-demo.py
+++ rx-demo.py
@@ -1,13 +1,13 @@
 from afskmodem import digitalReceiver
 from afskmodem import digitalModulationTypes
 receiver = digitalReceiver(digitalModulationTypes.default())
-print("NBFMmodem RX Demo")
+print("AFSKmodem RX Demo")
 while(True):
     print("Waiting for message...\n")
     while(True):
         rxData, errorCount = receiver.rx()
-        #if(rxData != b""):
-        print("Transmission received. " + str(errorCount) + " errors corrected.")
-        print(rxData.decode("ascii", "ignore"))
-        print("\nDone. (CTRL-C to exit)")
+        if(rxData != b""):
+            print("Transmission received. " + str(errorCount) + " errors corrected.")
+            print(rxData.decode("ascii", "ignore"))
+            print("\nDone. (CTRL-C to exit)")
         break
