5a7eb3d63ec825012365e149ec9f966ca0c3f203
Juno <jvmeifert@gmail.com>
2024-02-13 11:47:06 -0500

    cleanly handle interrupts in demos

--- rx-demo.py
+++ rx-demo.py
@@ -1,12 +1,21 @@
 from afskmodem import Receiver
-receiver = Receiver(1200)
-print("AFSKmodem RX Demo")
-while(True):
-    print("Waiting for message...\n")
+
+def main():
+    receiver = Receiver(1200)
+    print("AFSKmodem RX Demo")
     while(True):
-        rxData = receiver.receive(100)
-        if(rxData != b""):
-            print("Transmission received.")
-            print(rxData.decode("utf-8", "ignore"))
-            print("\nDone. (CTRL-C to exit)")
-        break
+        print("Waiting for message...\n")
+        while(True):
+            rxData = receiver.receive(100)
+            if(rxData != b""):
+                print("Transmission received.")
+                print(rxData.decode("utf-8", "ignore"))
+                print("\nDone. (CTRL-C to exit)")
+            break
+
+if(__name__ == "__main__"):
+    try:
+        main()
+    except(KeyboardInterrupt):
+        print("CTRL-C")
+        pass
--- tx-demo.py
+++ tx-demo.py
@@ -1,9 +1,18 @@
 from afskmodem import Transmitter
-transmitter = Transmitter(1200)
-print("AFSKmodem TX Demo")
-while True:
-    print("Enter message string (ASCII):")
-    userMessage = input()
-    print("Transmitting...")
-    transmitter.transmit(userMessage.encode("ascii", "ignore"))
-    print("Done. (CTRL-C to exit)\n")
+
+def main():
+    transmitter = Transmitter(1200)
+    print("AFSKmodem TX Demo")
+    while True:
+        print("Enter message string (ASCII):")
+        userMessage = input()
+        print("Transmitting...")
+        transmitter.transmit(userMessage.encode("ascii", "ignore"))
+        print("Done. (CTRL-C to exit)\n")
+
+if(__name__ == "__main__"):
+    try:
+        main()
+    except(KeyboardInterrupt):
+        print("CTRL-C")
+        pass
